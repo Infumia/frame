4f27f6edaf268fe4293ecb522f668255f8361d95
Hasan Demirtaş <utsukushihito@outlook.com>
2024-10-06 22:01:25 +0300

    Update PipelineBase.java

--- common/src/main/java/net/infumia/frame/pipeline/PipelineBase.java
+++ common/src/main/java/net/infumia/frame/pipeline/PipelineBase.java
@@ -1,13 +1,9 @@
 package net.infumia.frame.pipeline;
 
-import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
 import net.infumia.frame.service.Implementation;
 import net.infumia.frame.service.Service;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public interface PipelineBase<B extends PipelineContext, R, Self extends PipelineBase<B, R, Self>> {
     @NotNull
@@ -22,64 +18,5 @@ public interface PipelineBase<B extends PipelineContext, R, Self extends Pipelin
     @NotNull
     default Self register(@NotNull final Service<B, R> service) {
         return this.apply(Implementation.register(service));
-    }
-
-    @NotNull
-    default Self register(
-        @NotNull final Service<B, R> service,
-        @Nullable final Collection<Predicate<B>> filters
-    ) {
-        return this.apply(Implementation.register(service, filters));
-    }
-
-    @NotNull
-    default Self replace(
-        @NotNull final String serviceKey,
-        @NotNull final UnaryOperator<Service<B, R>> operation
-    ) {
-        return this.apply(Implementation.replace(serviceKey, operation));
-    }
-
-    @NotNull
-    default Self replace(
-        @NotNull final String serviceKey,
-        @NotNull final UnaryOperator<Service<B, R>> operation,
-        @Nullable final Collection<Predicate<B>> filters
-    ) {
-        return this.apply(Implementation.replace(serviceKey, operation, filters));
-    }
-
-    @NotNull
-    default Self registerAfter(
-        @NotNull final String serviceKey,
-        @NotNull final Service<B, R> service
-    ) {
-        return this.apply(Implementation.registerAfter(serviceKey, service));
-    }
-
-    @NotNull
-    default Self registerAfter(
-        @NotNull final String serviceKey,
-        @NotNull final Service<B, R> service,
-        @Nullable final Collection<Predicate<B>> filters
-    ) {
-        return this.apply(Implementation.registerAfter(serviceKey, service, filters));
-    }
-
-    @NotNull
-    default Self registerBefore(
-        @NotNull final String serviceKey,
-        @NotNull final Service<B, R> service
-    ) {
-        return this.apply(Implementation.registerBefore(serviceKey, service));
-    }
-
-    @NotNull
-    default Self registerBefore(
-        @NotNull final String serviceKey,
-        @NotNull final Service<B, R> service,
-        @Nullable final Collection<Predicate<B>> filters
-    ) {
-        return this.apply(Implementation.registerBefore(serviceKey, service, filters));
     }
 }
