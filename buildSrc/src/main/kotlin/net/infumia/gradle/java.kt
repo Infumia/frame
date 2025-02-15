f1f9b669bc6385aa25f0e41b4115665d63eddca3
Hasan Demirtaş <utsukushihito@outlook.com>
2025-02-15 15:02:20 +0300

    lib.

--- buildSrc/src/main/kotlin/net/infumia/gradle/java.kt
+++ buildSrc/src/main/kotlin/net/infumia/gradle/java.kt
@@ -1,6 +1,7 @@
 package net.infumia.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaLibraryPlugin
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.jvm.toolchain.JavaLanguageVersion
@@ -9,6 +10,7 @@ import org.gradle.kotlin.dsl.*
 
 fun Project.applyJava(javaVersion: Int = 8) {
     apply<JavaPlugin>()
+    apply<JavaLibraryPlugin>()
 
     repositories.mavenCentral()
 
