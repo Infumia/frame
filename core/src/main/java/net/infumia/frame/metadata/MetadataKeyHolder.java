eeed94a68d572038e78b4dde74f245d3ef463d76
Hasan Demirtaş <utsukushihito@outlook.com>
2025-04-23 07:34:37 +0300

    Update MetadataKeyHolder.java

--- core/src/main/java/net/infumia/frame/metadata/MetadataKeyHolder.java
+++ core/src/main/java/net/infumia/frame/metadata/MetadataKeyHolder.java
@@ -5,7 +5,11 @@ import java.util.Deque;
 import java.util.Map;
 import net.infumia.frame.context.view.ContextRender;
 import net.infumia.frame.typedkey.TypedKey;
+import net.infumia.frame.view.ViewEventHandler;
 import net.infumia.frame.viewer.ContextualViewer;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 
 public interface MetadataKeyHolder {
     TypedKey<ContextualViewer> CONTEXTUAL_VIEWER = TypedKey.of(
@@ -13,15 +17,29 @@ public interface MetadataKeyHolder {
         "contextual-viewer"
     );
 
+    /**
+     * The key for players transition from a frame to another frame inventory.
+     */
     TypedKey<ContextualViewer> TRANSITIONING_FROM = TypedKey.of(
         ContextualViewer.class,
         "transitioning-from"
     );
 
-    TypedKey<Boolean> TRANSITIONING = TypedKey.of(
-        Boolean.class,
-        "transitioning"
-    );
+    /**
+     * The key for players transition from any inventory to frame inventory.
+     * <p>
+     * This helps to recognize when a player tries to open a frame inventory while views a non-frame inventory.
+     * <p>
+     * The reason why this is important is because,
+     * Bukkit does not send {@link InventoryCloseEvent} before {@link InventoryOpenEvent} so,
+     * it creates bugs where while a player views a regular chest inventory then tries to open a frame inventory and,
+     * since {@link #TRANSITIONING_FROM} not exists in this stage,
+     * {@link #CONTEXTUAL_VIEWER} is get being used in InventoryListener#transitioningOrCurrent
+     * which is the newly opened frame inventory then frame thinks the frame inventory is closed however,
+     * for the player it's still opened and player can interact with everything
+     * without triggering the {@link InventoryClickEvent} for {@link ViewEventHandler}.
+     */
+    TypedKey<Boolean> TRANSITIONING = TypedKey.of(Boolean.class, "transitioning");
 
     TypedKey<Boolean> FORCED_CLOSE = TypedKey.of(boolean.class, "forced-close");
 
