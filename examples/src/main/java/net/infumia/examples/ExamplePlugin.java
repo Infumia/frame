ed3b6adfd8698d7e9ba3ed2a30faa7691c1758a6
Hasan Demirtaş <utsukushihito@outlook.com>
2025-06-02 01:38:14 +0300

    example.

---
+++ examples/src/main/java/net/infumia/examples/ExamplePlugin.java
@@ -0,0 +1,33 @@
+package net.infumia.examples;
+
+import net.infumia.frame.Frame;
+import net.infumia.frame.typedkey.TypedKey;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public final class ExamplePlugin extends JavaPlugin implements CommandExecutor {
+    public static final TypedKey<CommandSender> CONSOLE_KEY =
+        TypedKey.of(CommandSender.class, "console");
+
+    private final Frame frame = Frame.create(this)
+        .with(ViewExample.class);
+
+    @Override
+    public void onEnable() {
+        this.frame.register(builder ->
+            builder.add(ExamplePlugin.CONSOLE_KEY, Bukkit.getConsoleSender()));
+        this.getCommand("test").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command, @NotNull final String label, final String @NotNull [] args) {
+        Bukkit.getScheduler().runTaskAsynchronously(this, () ->
+            this.frame.open(((Player) sender), ViewExample.class));
+        return true;
+    }
+}
---
+++ examples/src/main/java/net/infumia/examples/ViewExample.java
@@ -0,0 +1,45 @@
+package net.infumia.examples;
+
+import net.infumia.frame.context.view.ContextInit;
+import net.infumia.frame.context.view.ContextOpen;
+import net.infumia.frame.context.view.ContextRender;
+import net.infumia.frame.type.InvType;
+import net.infumia.frame.view.ViewHandler;
+import net.infumia.frame.viewer.Viewer;
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+
+public final class ViewExample implements ViewHandler {
+
+    @Override
+    public void onInit(final ContextInit ctx) {
+        ctx.configBuilder().type(InvType.CHEST).cancelOnClick();
+    }
+
+    @Override
+    public void onOpen(final ContextOpen ctx) {
+        final Viewer viewer = ctx.viewer();
+        ctx
+            .modifyConfig()
+            .layout(new String[]{
+                    "xxxxxxxxx",
+                    "xxxxaxxxx",
+                    "xxxxxxxxx"
+            })
+            .title("Player: " + viewer.player().getName());
+    }
+
+    @Override
+    public void onFirstRender(final ContextRender ctx) {
+        final CommandSender sender = ctx.instances().getOrThrow(ExamplePlugin.CONSOLE_KEY);
+        ctx.layoutSlot('x', new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
+
+        ctx
+            .layoutSlot('a', new ItemStack(Material.DIAMOND))
+            .onClick(context -> {
+                context.closeForViewer();
+                sender.sendMessage("Player " + context.clicker().player() + " clicked to a diamond!");
+            });
+    }
+}
