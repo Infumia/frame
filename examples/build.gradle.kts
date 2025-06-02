d86957ba34e31f422063d6b96903c701af94de37
Hasan Demirtaş <utsukushihito@outlook.com>
2025-06-02 15:41:34 +0300

    Update build.gradle.kts

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -9,14 +9,14 @@ group = "net.infumia"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://central.sonatype.com/repository/maven-snapshots/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    implementation("net.infumia:frame:0.2.12-SNAPSHOT")
+    implementation("net.infumia:frame:1.0.0-SNAPSHOT")
     implementation("io.leangen.geantyref:geantyref:1.3.16")
 }
 
