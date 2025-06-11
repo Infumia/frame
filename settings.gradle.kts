720837618a2808788658798908d614a50155ee67
Hasan Demirtaş <utsukushihito@outlook.com>
2025-06-11 15:08:00 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,6 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 
 rootProject.name = "frame"
 
-include(
-    "common",
-    "injector",
-    "service",
-    "api",
-    "core",
-)
+include("common", "injector", "service", "api", "core")
 
-// include("config", "annotations", "annotation")
+include("config", "annotations", "annotation")
