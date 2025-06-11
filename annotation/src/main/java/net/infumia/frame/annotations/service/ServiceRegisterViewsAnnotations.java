e331989d6e0db516536086a3dd53187d914b5b76
Hasan Demirtaş <utsukushihito@outlook.com>
2025-06-11 16:07:40 +0300

    Update ServiceRegisterViewsAnnotations.java

--- annotation/src/main/java/net/infumia/frame/annotations/service/ServiceRegisterViewsAnnotations.java
+++ annotation/src/main/java/net/infumia/frame/annotations/service/ServiceRegisterViewsAnnotations.java
@@ -5,7 +5,7 @@ import java.util.function.BiConsumer;
 import net.infumia.frame.pipeline.context.PipelineContextFrame;
 import net.infumia.frame.view.View;
 
-public final class ServiceRegisterViewsInitializeAnnotations
+public final class ServiceRegisterViewsAnnotations
     implements BiConsumer<PipelineContextFrame.RegisterViews, Collection<View>> {
 
     @Override
