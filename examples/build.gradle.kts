b18891ca8dab43043231eaf2ac8d914c68418e49
Hasan Demirtaş <utsukushihito@outlook.com>
2025-08-14 13:40:36 +0300

    Update Gradle wrapper and dependencies
    Upgraded Gradle wrapper to 9.0.0 and updated related scripts for improved compatibility and reliability. Updated plugin and dependency versions in build.gradle.kts, set Java toolchain to Adoptium Java 21, and added the Foojay resolver plugin in settings.gradle.kts.

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,12 +1,19 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.0.0-beta13"
+    id("com.gradleup.shadow") version "9.0.1"
     id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "net.infumia"
 
 version = "1.0.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.ADOPTIUM
+    }
+}
 
 repositories {
     mavenLocal()
@@ -15,14 +22,16 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+
     implementation("net.infumia:frame:1.0.0-SNAPSHOT")
-    implementation("io.leangen.geantyref:geantyref:1.3.16")
+    implementation("net.infumia:title-updater:0.1.3")
+    implementation("io.leangen.geantyref:geantyref:2.0.1")
 }
 
 tasks {
     runServer {
-        minecraftVersion("1.21.4")
+        minecraftVersion("1.21.8")
         jvmArgs("-Dcom.mojang.eula.agree=true")
         downloadPlugins { github("jpenilla", "TabTPS", "v1.3.27", "tabtps-spigot-1.3.27.jar") }
     }
