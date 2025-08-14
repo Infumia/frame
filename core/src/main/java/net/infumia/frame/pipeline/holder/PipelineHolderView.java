6d7d9e1e575c6b9a1c2bb927f87c82bd141137ee
Hasan Demirtaş <utsukushihito@outlook.com>
2025-08-14 18:37:28 +0300

    Update PipelineHolderView.java

--- core/src/main/java/net/infumia/frame/pipeline/holder/PipelineHolderView.java
+++ core/src/main/java/net/infumia/frame/pipeline/holder/PipelineHolderView.java
@@ -31,6 +31,7 @@ import net.infumia.frame.pipeline.service.view.ServiceInitWaitUntil;
 import net.infumia.frame.pipeline.service.view.ServiceLayoutResolution;
 import net.infumia.frame.pipeline.service.view.ServiceLayoutResolutionLogging;
 import net.infumia.frame.pipeline.service.view.ServiceModifyContainerLogging;
+import net.infumia.frame.pipeline.service.view.ServiceOpenInitializePagination;
 import net.infumia.frame.pipeline.service.view.ServiceOpenInitializeState;
 import net.infumia.frame.pipeline.service.view.ServiceOpenLogging;
 import net.infumia.frame.pipeline.service.view.ServiceOpenOnOpen;
@@ -81,6 +82,7 @@ public final class PipelineHolderView implements Cloned<PipelineHolderView> {
             ServiceOpenLogging.INSTANCE
         )
             .register(ServiceOpenWaitUntil.INSTANCE)
+            .register(ServiceOpenInitializePagination.INSTANCE)
             .register(ServiceOpenOnOpen.INSTANCE)
             .register(ServiceOpenInitializeState.INSTANCE)
             .register(ServiceOpenPreviousView.INSTANCE),
